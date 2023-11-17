ae144bb6bbd21378e8c6dd31b2340d7172a11b64
paulosjnr <108819557+paulosjnr@users.noreply.github.com>
2023-11-17 20:22:36 -0300

    Add Game Controller

--- src/main/java/com/example/dslist/services/GameService.java
+++ src/main/java/com/example/dslist/services/GameService.java
@@ -15,8 +15,8 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
-	public List<Game> findAll(){
+	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
-		return result;
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
